f38787b635c5debfd8f4ec6df0e049e837de66d8
shinichi <basty.getuiza@lorma.edu>
2026-01-13 14:05:29 +0800

    initialize

---
+++ Array1.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class Array1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] values = {21,23,19,4,67,76,3,20,31,1};
+
+        for (int i = 0; i < values.length; i++){
+            System.out.println(values[i]);
+        }
+
+        System.out.println("enter a number from 1 - 10: ");
+        int user = sc.nextInt();
+        
+        for (int i = 0; i < values.length; i++){
+            if (values [i] == user){
+                System.out.println(i +  1);
+            }
+            
+        }
+
+    }
+    
+}
