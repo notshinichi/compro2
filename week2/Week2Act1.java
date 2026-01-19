dc7f9a26e2dbef602c9800267f6c51c0fe00a805
notshinichi <basty.getuiza@lorma.edu>
2026-01-19 14:05:04 +0800

    week2act1

--- week2/Week2Act1.java
+++ week2/Week2Act1.java
@@ -1,5 +1,26 @@
 package week2;
 
 public class Week2Act1 {
+    public static void main(String[] args) {
+        
     
+        int[] theaterRow = {0,0,0,1,0,0,0,0};
+
+        
+
+        for(int r = 0; r < theaterRow.length; r++ ){
+            System.out.print(theaterRow[r] + " "); 
+        }
+
+        int availaleSeats = 0;
+        for (int i = 0; i < theaterRow.length; i++){
+            if (theaterRow[i] == 0){
+                availaleSeats++;
+            }
+        }
+        System.out.println("\nThere are " + availaleSeats + " left.");
+
+        
+        
+    }
 }
